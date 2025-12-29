445f6e7bd0273f6683cb479370e2f84c7bc416ef
yearofthedan <10258529+yearofthedan@users.noreply.github.com>
2025-12-29 18:24:12 +0100

    Add missing style import for VueFlow controls

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,4 +1,6 @@
 export default {
   '*': (stagedFiles) => [`sh ./do lint ${stagedFiles.join(' ')}`, 'sh ./do build'],
-  'src/**/*.{js,ts,tsx,vue}': (stagedFiles) => [`sh ./do test ${stagedFiles.join(' ')}`],
+  'src/**/*.{js,ts,tsx,vue}': (stagedFiles) => [
+    `sh ./do test --passWithNoTests ${stagedFiles.join(' ')}`,
+  ],
 }
