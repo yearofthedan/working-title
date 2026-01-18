5a074661003d9815de9b4c5682f55e8e8cec3036
yearofthedan <10258529+yearofthedan@users.noreply.github.com>
2026-01-18 10:59:04 +0000

    Increase memory to container, node process. Tweak test concurrency

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,6 +1,6 @@
 export default {
   '*': (stagedFiles) => [`sh ./do lint ${stagedFiles.join(' ')}`, 'sh ./do build'],
   'src/**/*.{js,ts,tsx,vue}': (stagedFiles) => [
-    `sh ./do test --passWithNoTests ${stagedFiles.join(' ')}`,
+    `sh ./do test related --passWithNoTests ${stagedFiles.join(' ')}`,
   ],
 }
