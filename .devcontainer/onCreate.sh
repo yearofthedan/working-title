ecf399ae42d7ca0b32414fdc7cb9392ada25cdc6
yearofthedan <10258529+yearofthedan@users.noreply.github.com>
2026-02-02 08:56:29 +0000

    build: Use a volume for the workspace rather than a bind mount to the host
    Significantly faster performance by not writing changes back to the mac host

---
+++ .devcontainer/onCreate.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+# Fix permissions on folders after volumes are mounted
+sudo chown -R node:node /home/node/.vscode-server ${containerWorkspaceFolder}
+
+# Pull code 
+git clone https://github.com/yearofthedan/working-title.git .
